0ac8720f107fcfa1154b016c1e81a4e2e24aa184
Martin Kemp <martinkemp@deloitte.co.uk>
2020-01-15 13:45:30 +0000

    Don't use aws-sdk-mock, it's broken

--- tests/unit/cloudwatch.unit.test.ts
+++ tests/unit/cloudwatch.unit.test.ts
@@ -1,46 +1,29 @@
-import sinon = require("sinon");
-import * as AWSMock from "aws-sdk-mock";
-import AWS = require("aws-sdk");
 import { CW } from "../../src/cloudwatch";
+import { CloudWatch } from "aws-sdk";
 
-const sandbox = sinon.createSandbox();
 describe("The CloudWatch class", () => {
-    let cw: CW;
-    beforeEach(() => {
-        cw = new CW();
-    });
-    afterEach(() => sandbox.restore());
+  const cw = new CW();
+  jest.mock("aws-sdk/clients/cloudwatch");
+  const mockFn = jest.fn().mockImplementation(() => ({ promise: () => ({}) }));
+  CloudWatch.prototype.putMetricData = mockFn;
+  process.env.BRANCH = "local";
 
-    it("should send visit metrics", async () => {
-        const visitsToday = 42;
-        const oldVisits = 0;
-        const openVisits = 5;
-        const pSpy = sinon.fake.resolves({});
-        AWSMock.setSDKInstance(AWS);
-        AWSMock.mock("CloudWatch", "putMetricData", pSpy);
-        const res = await cw.sendVisits(visitsToday, oldVisits, openVisits);
-        expect(res).toBe(`visits: ${visitsToday}, oldVisits: ${oldVisits}, openVisits: ${openVisits}`);
-        expect(pSpy.calledOnce).toBeTruthy();
-        AWSMock.restore("CloudWatch");
-    });
+  it("should send visit metrics", async () => {
+    const [visitsToday, oldVisits, openVisits] = [42, 0, 5];
+    const res = await cw.sendVisits(visitsToday, oldVisits, openVisits);
+    expect(res).toBe(`visits: ${visitsToday}, oldVisits: ${oldVisits}, openVisits: ${openVisits}`);
+    expect(mockFn).toHaveBeenCalled();
+  });
 
-    it("should send timeout metrics", async () => {
-        const pSpy = sinon.fake.resolves({});
-        AWSMock.setSDKInstance(AWS);
-        AWSMock.mock("CloudWatch", "putMetricData", pSpy);
-        const res = await cw.sendTimeouts("testGroup", [{ id: "asdf", timestamp: 0, message: "[ERROR] Task timed out" }]);
-        expect(res).toBe("testGroup: 1");
-        expect(pSpy.calledOnce).toBeTruthy();
-        AWSMock.restore("CloudWatch");
-    });
+  it("should send timeout metrics", async () => {
+    const res = await cw.sendTimeouts("testGroup", [{ id: "asdf", timestamp: 0, message: "[ERROR] Task timed out" }]);
+    expect(res).toBe("testGroup: 1");
+    expect(mockFn).toHaveBeenCalled();
+  });
 
-    it("should send timeout metrics even when none returned", async () => {
-        const pSpy = sinon.fake.resolves({});
-        AWSMock.setSDKInstance(AWS);
-        AWSMock.mock("CloudWatch", "putMetricData", pSpy);
-        const res = await cw.sendTimeouts("testGroup", [{ id: "asdf", timestamp: 0, message: "[ERROR] Fatal error" }]);
-        expect(res).toBe("testGroup: 0");
-        expect(pSpy.calledOnce).toBeTruthy();
-        AWSMock.restore("CloudWatch");
-    });
+  it("should send timeout metrics even when none returned", async () => {
+    const res = await cw.sendTimeouts("testGroup", [{ id: "asdf", timestamp: 0, message: "[ERROR] Fatal error" }]);
+    expect(res).toBe("testGroup: 0");
+    expect(mockFn).toHaveBeenCalled();
+  });
 });
--- tests/unit/handler.unit.test.ts
+++ tests/unit/handler.unit.test.ts
@@ -1,64 +1,63 @@
 import { handler } from "../../src/handler";
-import mockContext = require("aws-lambda-mock-context");
-import sinon = require("sinon");
 import { CloudWatchLogsDecodedData } from "aws-lambda";
 import { gzip } from "node-gzip";
 import { Dynamo } from "../../src/dynamodb";
 import { CW } from "../../src/cloudwatch";
+import mockContext = require("aws-lambda-mock-context");
 
 
-const sandbox = sinon.createSandbox();
 const event: CloudWatchLogsDecodedData = {
-    messageType: "DATA_MESSAGE",
-    owner: "123456789123",
-    logGroup: "testLogGroup",
-    logStream: "testLogStream",
-    subscriptionFilters: ["testFilter"],
-    logEvents: [
-        { id: "eventId1", timestamp: 1440442987000, message: "[ERROR] First test message" },
-        { id: "eventId2", timestamp: 1440442987001, message: "[ERROR] Second test message" }
-    ]
+  messageType: "DATA_MESSAGE",
+  owner: "123456789123",
+  logGroup: "testLogGroup",
+  logStream: "testLogStream",
+  subscriptionFilters: ["testFilter"],
+  logEvents: [
+    { id: "eventId1", timestamp: 1440442987000, message: "[ERROR] First test message" },
+    { id: "eventId2", timestamp: 1440442987001, message: "[ERROR] Second test message" }
+  ]
 };
 
 async function encodeEvent(ev: CloudWatchLogsDecodedData) {
-    return (await gzip(JSON.stringify(ev))).toString("base64");
+  return (await gzip(JSON.stringify(ev))).toString("base64");
 }
 
+const getVisitsMock = jest.fn().mockImplementation(() => 0);
+const getOldVisitsMock = jest.fn().mockImplementation(() => 0);
+const getOpenVisitsMock = jest.fn().mockImplementation(() => 0);
+const sendVisitsMock = jest.fn().mockImplementation(() => "visits: 0, oldVisits: 0");
+let sendTimeoutsMock = jest.fn().mockImplementation(() => "testLogGroup: 0");
+
 describe("The lambda handler", () => {
-    const ctx = mockContext();
-    describe("with a valid event", () => {
-        afterEach(() => sandbox.restore());
-        it("should handle the incoming event", async () => {
-            const getVisitsStub = sandbox.stub(Dynamo.prototype, "getVisits").resolves(0);
-            const getOldVisitsStub = sandbox.stub(Dynamo.prototype, "getOldVisits").resolves(0);
-            const sendVisitsStub = sandbox.stub(CW.prototype, "sendVisits").resolves("visits: 0, oldVisits: 0");
-            const sendTimeoutsStub = sandbox.stub(CW.prototype, "sendTimeouts").resolves("testLogGroup: 0");
-            await handler({ awslogs: { data: await encodeEvent(event) } }, ctx, () => {
-                return;
-            });
-            sandbox.assert.notCalled(getVisitsStub);
-            sandbox.assert.notCalled(getOldVisitsStub);
-            sandbox.assert.notCalled(sendVisitsStub);
-            sandbox.assert.calledWith(sendTimeoutsStub, event.logGroup, event.logEvents);
-        });
+  process.env.BRANCH = "local";
+  const ctx = mockContext();
+  Dynamo.prototype.getVisits = getVisitsMock;
+  Dynamo.prototype.getOldVisits = getOldVisitsMock;
+  Dynamo.prototype.getOpenVisits = getOpenVisitsMock;
+  CW.prototype.sendVisits = sendVisitsMock;
+  CW.prototype.sendTimeouts = sendTimeoutsMock;
+
+  describe("with a valid event", () => {
+    it("should handle the incoming event", async () => {
+      await handler({ awslogs: { data: await encodeEvent(event) } }, ctx, () => void 0);
+      expect(getVisitsMock).not.toHaveBeenCalled();
+      expect(getOldVisitsMock).not.toHaveBeenCalled();
+      expect(getOpenVisitsMock).not.toHaveBeenCalled();
+      expect(sendTimeoutsMock).toHaveBeenCalledWith(event.logGroup, event.logEvents);
     });
-    describe("when it is handling the activities logs", () => {
-        afterEach(() => sandbox.restore());
-        const act: CloudWatchLogsDecodedData = { ...event };
-        act.logGroup = "/aws/lambda/activities-develop";
-        it("should retrieve the visit stats", async () => {
-            const getVisitsStub = sandbox.stub(Dynamo.prototype, "getVisits").resolves(0);
-            const getOldVisitsStub = sandbox.stub(Dynamo.prototype, "getOldVisits").resolves(0);
-            const sendVisitsStub = sandbox.stub(CW.prototype, "sendVisits").resolves("visits: 0, oldVisits: 0");
-            const sendTimeoutsStub = sandbox.stub(CW.prototype, "sendTimeouts").resolves("/aws/lambda/activities-develop: 0");
-            await handler({ awslogs: { data: await encodeEvent(act) } }, ctx, () => {
-                return;
-            });
-            sandbox.assert.called(getVisitsStub);
-            sandbox.assert.called(getOldVisitsStub);
-            sandbox.assert.called(sendVisitsStub);
-            sandbox.assert.calledWith(sendTimeoutsStub, act.logGroup, act.logEvents);
-        });
+  });
+  describe("when it is handling the activities logs", () => {
+    const act: CloudWatchLogsDecodedData = { ...event };
+    act.logGroup = "/aws/lambda/activities-develop";
+    sendTimeoutsMock = jest.fn().mockImplementation(() => "/aws/lambda/activities-develop: 0");
+    CW.prototype.sendTimeouts = sendTimeoutsMock;
+    it("should retrieve the visit stats", async () => {
+      await handler({ awslogs: { data: await encodeEvent(act) } }, ctx, () => void 0);
+      expect(getVisitsMock).toHaveBeenCalled();
+      expect(getOldVisitsMock).toHaveBeenCalled();
+      expect(getOpenVisitsMock).toHaveBeenCalled();
+      expect(sendTimeoutsMock).toHaveBeenCalledWith(act.logGroup, act.logEvents);
     });
+  });
 });
 
